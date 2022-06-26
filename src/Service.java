0ebe48fb406d80cf38f4d1008b925701419edaee
Karypbaeva Amina <karypbaeva.amina59@gmail.com>
2022-06-27 01:55:42 +0600

    init2

---
+++ src/Service.java
@@ -0,0 +1,63 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Service implements  Connectable {
+
+
+    @Override
+    public void openConnection() {
+
+    }
+
+    @Override
+    public void closeDataBaseGonnection() {
+
+    }
+
+    @Override
+    public void checkBaseConnection() {
+
+    }
+
+    @Override
+    public void readString() {
+
+    }
+
+    @Override
+    public void keyRecord() {
+
+    }
+
+    @Override
+    public void readKeyRecord() {
+
+    }
+
+    @Override
+    public void readSomeEntries() {
+
+    }
+
+    @Override
+    public void EntriesNumber() {
+
+    }
+
+    @Override
+    public void addNote() {
+
+    }
+
+    @Override
+    public void entryIndexUpdate() {
+
+    }
+
+    @Override
+    public void contentKey() {
+
+    }
+}
